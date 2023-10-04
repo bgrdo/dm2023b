1f5185911bb3024153128eeeb4f87991df684f4d
bgrdo <belu.girardo@gmail.com>
2023-10-03 21:40:36 -0300

    Exp 6

--- src/ArbolesAzarosos/321_arboles_azarosos_002.r
+++ src/ArbolesAzarosos/321_arboles_azarosos_002.r
@@ -11,7 +11,7 @@ require("rpart")
 
 # parmatros experimento
 PARAM <- list()
-PARAM$experimento <- 3210
+PARAM$experimento <- 3210-100-percent
 
 # Establezco la semilla aleatoria, cambiar por SU primer semilla
 PARAM$semilla <- 199999
@@ -19,17 +19,17 @@ PARAM$semilla <- 199999
 # parameetros rpart
 PARAM$rpart_param <- list(
   "cp" = -1,
-  "minsplit" = 50,
-  "minbucket" = 5,
-  "maxdepth" = 10
+  "minsplit" = 250,
+  "minbucket" = 100,
+  "maxdepth" = 14
 )
 
 # parametros  arbol
 # entreno cada arbol con solo 50% de las variables variables
-PARAM$feature_fraction <- 0.5
+PARAM$feature_fraction <- 1
 # voy a generar 500 arboles, a mas arboles mas tiempo de proceso y MEJOR MODELO
 #  pero ganancias marginales
-PARAM$num_trees_max <- 500
+PARAM$num_trees_max <- 1
 
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
@@ -52,7 +52,7 @@ setwd(carpeta_experimento)
 
 
 # que tamanos de ensemble grabo a disco, pero siempre debo generar los 500
-grabar <- c(1, 5, 10, 50, 100, 200, 500)
+grabar <- c(1)
 
 
 # defino los dataset de entrenamiento y aplicacion
